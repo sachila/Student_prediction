2dac17d7d6f2b0280bb9b4441e44fae49b296c8f
CORPNET\sacrlk <sachila.ranawaka@ifsworld.com>
2019-07-25 15:16:45 +0530

    add client

---
+++ client/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,16 @@
+import { Routes } from "@angular/router";
+
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { MapComponent } from "../../pages/map/map.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+import { TypographyComponent } from "../../pages/typography/typography.component";
+// import { RtlComponent } from "../../pages/rtl/rtl.component";
+
+export const AdminLayoutRoutes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "user", component: UserComponent }
+  // { path: "rtl", component: RtlComponent }
+];
